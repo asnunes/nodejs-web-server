9321cb33fa0e278dde98914ec0b1a90f31333b88
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2018-12-09 23:02:27 -0200

    Not found case added

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const http = require('http');
 const fs = require('fs')
 
 http.createServer((req, res)=> {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(`${__dirname}/index.htm`).pipe(res)
+    if (req.url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(`${__dirname}/index.htm`).pipe(res)
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('Not Found');
+    }
 }).listen(1337, '127.0.0.1');
